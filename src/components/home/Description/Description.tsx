da37ba71166fea32ab3277821cdc69d471103386
Guillermo García López <memogl25@gmail.com>
2025-04-07 22:01:35 -0600

    feat(Description): add product marketplace image and description text

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,9 @@
 export const Description = () => {
   return (
     <section>
-      <h1>Descripción</h1>
+      <img src="/images/description.jpeg" alt="products marketplace" />
+      <h2>Description</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
     </section>
   )
 } 
