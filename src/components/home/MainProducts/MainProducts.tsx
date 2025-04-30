a58234b876d8504c59a60ddf4ab1c5a468f2d883
Guillermo García López <memogl25@gmail.com>
2025-04-30 07:44:30 -0600

    feat(env): add example environment variables and update .gitignore to exclude .env file

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 export const MainProducts = () => {
+  console.log(process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
+  console.log(process.env.SHOPIFY_API_KEY);
+
   return (
     <section>
       <h1>MainProducts</h1>
     </section>
   )
-} 
+}
