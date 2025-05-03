675f24d165f491d0b4e4005984ed1e715e001245
Guillermo García López <memogl25@gmail.com>
2025-05-02 21:38:42 -0600

    feat(store): enhance layout and styling for collections; implement collection-specific product fetching

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
 import { getProducts } from "app/services/shopify/products"
+import { getCollections, getCollectionProducts } from "app/services/shopify/collections"
 
 interface CategoryProps {
   params: {
@@ -9,9 +10,17 @@ interface CategoryProps {
 }
 
 export default async function Category(props: CategoryProps) {
-  const products = await getProducts()
   const { categories } = props.params
+  let products = []
+  const collections = await getCollections()
 
+  if (categories?.length > 0) {
+    const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
+    products = await getCollectionProducts(selectedCollectionId)
+  } else {
+    products = await getProducts()
+  }
+  
   return (
     <ProductsWrapper products={products} />
   )
