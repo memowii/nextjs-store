e3a465014baf73dfec9ce82e6ed4b5fa6f83559c
Guillermo García López <memogl25@gmail.com>
2025-05-06 07:53:03 -0600

    feat(signup): implement user creation logic in handleCreateUser and update form submission handling in NewAccountForm

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,19 @@
 "use server";
+import { GraphQLClientSingleton } from "app/graphql";
+import { createUserMutation } from "app/graphql/mutations/createUserMutation";
 
-export const handleCreateUser = (formData: FormData) => {
-  console.log('formData', formData)
+export const handleCreateUser = async (formData: FormData) => {
+  const formDataObject = Object.fromEntries(formData)
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  delete formDataObject.password_confirmation
+  const variables = {
+    input: {
+      ...formDataObject,
+      phone: '+52' + formDataObject.phone,
+    }
+  }
+
+  const data = await graphqlClient.request(createUserMutation, variables)
+  console.log(data)
+  // return data
 }
