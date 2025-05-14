2cb01d00623d6d4b0543f41b51bc2d6ed0189942
Guillermo García López <memogl25@gmail.com>
2025-05-14 07:30:37 -0600

    feat(cart): enhance ProductView and ShoppingCart components with addToCart functionality and display cart items

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -2,16 +2,32 @@
 import { useState } from 'react'
 import { FaShoppingCart } from 'react-icons/fa'
 import styles from './ShoppingCart.module.sass'
+import { useShoppingCart } from 'app/hooks/useShoppingCart'
 
 export const ShoppingCart = () => {
-  const [countItems, setCountItems] = useState(0)
+  const { cart } = useShoppingCart()
+  const [isOpen, setIsOpen] = useState(false)
+  const handleOpen = () => setIsOpen(!isOpen)
   
   return (
-    <button className={styles.ShoppingCart}>
+    <button className={styles.ShoppingCart} onClick={handleOpen}>
       <span className={styles.ShoppingCart__counter}>
-        {countItems}
+        { cart.length }
       </span>
       <FaShoppingCart />
+      {isOpen && (
+        <div className={styles.ShoppingCart__items}>
+          {cart.map(item => (
+            <>
+              <p key={item?.id}>{item?.title}</p>
+              <p>Cantidad: {item.quantity}</p>
+            </>
+          ))}
+          <button className={styles.ShoppingCart__buyButton}>
+            Buy
+          </button>
+        </div>
+      )}
     </button>
   )
 }
