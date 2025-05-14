2cf637886dfd6688966baf3fa510d93499ea1df8
Guillermo García López <memogl25@gmail.com>
2025-05-14 08:04:45 -0600

    refactor(cart): convert ShoppingCart to default export, implement NoSSR for improved performance in Header, and enhance createAccessToken function with better error handling

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
-import { ShoppingCart } from 'app/components/shared/ShoppingCart'
 import { validateAccessToken } from 'app/utils/auth/validateAccessToken'
 import styles from './Header.module.sass'
+import dynamic from 'next/dynamic'
 
+const NoSSRShoppingCart = dynamic(() => import('../ShoppingCart'), { ssr: false })
 
 export const Header = async () => {
   const customer = await validateAccessToken()
@@ -25,7 +26,7 @@ export const Header = async () => {
       </nav>
       <div className={styles.Header__user}>
         {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
-        <ShoppingCart />
+        <NoSSRShoppingCart />
       </div>
     </header>)
 }
